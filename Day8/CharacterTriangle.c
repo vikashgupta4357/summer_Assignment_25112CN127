ad21ab87c9f6e94a4d028ec84a9e210b91d4a5d2
Vikash Gupta <vikaskasaudhan4357@gmail.com>
2026-06-25 21:18:58 +0530

    Add files via upload

---
+++ Day8/CharacterTriangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int i,j,n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    for(i=1 ;i<=n ;i++){
+        int a = 1;
+        for(j=1 ;j<=i ;j++){
+            int d = a+64;
+            char ch = (char)d;
+            printf("%c ",ch);
+            a++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
